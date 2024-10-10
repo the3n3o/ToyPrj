34e950e3058f244e624aaa6f04741c0d89703d95
n3o <the3n3o@gmail.com>
2024-10-10 22:01:37 +0900

    05 - Quiz_8 Add HighScores

--- toy-projects/05-quiz/js/end.js
+++ toy-projects/05-quiz/js/end.js
@@ -3,6 +3,9 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.querySelector('#finalScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,4 +15,15 @@ username.addEventListener('keyup', () => {
 saveHighScore = (event) => {
   console.log('clicked the save button!');
   event.preventDefault();
-}
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a,b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('./');
+};
